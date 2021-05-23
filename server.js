eeba4c456bb726f59658ec291dcbcfe9ad50b2fa
Nick Verneck <nicollas@gmail.com>
2021-05-23 17:58:20 -0400

    added controllers/index and home routes

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(require("./controllers"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/UCF',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+app.listen(PORT, () => {
+    console.log(`App running on  http://localhost:${PORT}!`);
+  });
