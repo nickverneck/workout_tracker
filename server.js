c668460ec5dfa25ee03782abd2135cbf47a66607
Nick Verneck <nicollas@gmail.com>
2021-05-23 19:47:10 -0400

    addeed env configs to make it work on herokuy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config();
 // heroku port or 3000
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.static("public"));
 app.use(require("./controllers"));
 // connecting to database
 mongoose.connect(
-  process.env.MONGODB_URL || 'mongodb://localhost/UCF',
+  process.env.MONGODB_URL ,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
